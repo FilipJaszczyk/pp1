027104f55af76260d8b4e590d11838ccb5a5152b
Filip Jaszczyk <filipqwerty32@gmail.com>
2019-10-14 22:32:39 +0200

    Added Exercies 01

---
+++ 01-TypesAndVariables/Exercies 01/random_1.py
@@ -0,0 +1,9 @@
+from random import randint
+#Generating numbers
+x = randint(1,6)
+y = randint(1,6)
+z = randint(1,6)
+#Sum of numbers
+throws_sum = x + y + z
+#Show results
+print(f'The result of 3 throws: {x} {y} {z} \nSum: {throws_sum}')
